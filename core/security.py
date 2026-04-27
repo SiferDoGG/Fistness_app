ba7b26935ff1599681e23b9d9a4fc1c536815870
Roman Korchagin <sifer.roman@mail.ru>
2026-04-27 19:06:48 +0700

    add jose in security and rework datetime

--- core/security.py
+++ core/security.py
@@ -1,6 +1,7 @@
 from passlib.context import CryptContext
-from jose import jwt, JWTError
-from datetime import datetime, timedelta
+from jose import JWTError
+from jose.jwt import encode, decode
+from datetime import datetime, timedelta, timezone
 from fastapi import HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 
@@ -27,11 +28,11 @@ def verify_password(plain: str, hashed: str) -> bool:
 def create_access_token(data: dict) -> str:
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
 
     to_encode.update({"exp": expire})
 
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 # ---------------- CURRENT USER ----------------
@@ -39,7 +40,7 @@ def create_access_token(data: dict) -> str:
 
 def decode_token(token: str):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        payload = decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except JWTError:
         return None
