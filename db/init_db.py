ac6d42358884cb027073291e10d86c099ec2e61d
Roman Korchagin <sifer.roman@mail.ru>
2026-05-04 14:49:30 +0700

    delete comments

--- db/init_db.py
+++ db/init_db.py
@@ -1,7 +1,6 @@
 from db.base import Base
 from db.session import engine
 
-# Импорты моделей обязательны, чтобы SQLAlchemy зарегистрировал их в metadata
 from models.user import User  # noqa: F401
 from models.exercise import Exercise  # noqa: F401
 from models.workout import Workout  # noqa: F401
