59499fc097953baf57004f09a7d0006c9c0e2f44
Roman Korchagin <sifer.roman@mail.ru>
2026-05-08 22:37:25 +0700

    added getuser in users endpoint

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+
+from dependencies.auth import get_current_user
+from models.user import User
+from schemas.user import UserRead
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -7,6 +11,7 @@ router = APIRouter(prefix="/users", tags=["users"])
     "/me",
     summary="Получить информацию о текущем пользователе",
     description="Возвращает информацию о текущем вошедшем пользователе",
+    response_model=UserRead,
 )
-async def get_user():
-    pass
+async def get_user(curr_user: User = Depends(get_current_user)):
+    return curr_user
