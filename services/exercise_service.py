a1ab75ee5335b6875b85195b71f106701d3b6a6d
Roman Korchagin <sifer.roman@mail.ru>
2026-05-12 00:46:15 +0700

    update create_exercise_service func

--- services/exercise_service.py
+++ services/exercise_service.py
@@ -1,12 +1,18 @@
 from repositories.exercise_repo import (
     create_exercise,
     get_exercise_by_id,
+    get_exercise_by_name,
     get_exercises,
 )
 from fastapi import HTTPException
 
 
 async def create_exercise_service(db, data):
+    exercise = await get_exercise_by_name(db, data.name)
+    if exercise:
+        raise HTTPException(
+            status_code=400, detail="Exercise with this name already exists"
+        )
     return await create_exercise(db, data)
 
 
